ebe2e8c990582748aa4c108ef41b15c4a96cdfd1
laki-spk <142237202+laki-spk@users.noreply.github.com>
2024-03-26 11:29:32 -0400

    Add files via upload

---
+++ Split_WT_KO/run_AFmultiomeN_1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Sample batchscript to run a parallel python job on HPC using 10 CPU cores
+ 
+#SBATCH --partition=bch-compute                                # queue to be used
+#SBATCH --time=9-20:05:00                         # Running time (in hours-minutes-seconds)
+#SBATCH --job-name=perturbationsim                        # Job name
+#SBATCH --mail-type=BEGIN,END,FAIL              # send and email when the job begins, ends or fails
+#SBATCH --mail-user=lakindu.pathirakankanamge@childrens.harvard.edu          # Email address to send the job status
+#SBATCH --output=output_%j.txt                  # Name of the output file
+#SBATCH --nodes=1                            # Number of compute nodes
+#SBATCH --ntasks=10                         # Number of cpu cores on one node
+#SBATCH --mem=900G
+
+echo "Hello BCH user $(whoami)!"
+echo "This is a message from E2 node $(hostname) sent at $(date)"
+
+
+
+cd /temp_work/ch250798/Scenic
+
+python NFYA1.py 
